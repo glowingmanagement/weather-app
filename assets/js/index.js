276eb954a76dc44841e29e2550d3671bbac81027
Josh Holmes <josh@glowingmanagement.com>
2022-05-10 16:53:44 +0100

    call to API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,29 @@
 const searchInput = $("#search-input");
+const apiKey = "59d35777f6ec5554a91df4c08291fafc";
 
-const handleFormSubmit = (event) => {
-  event.preventDefault();
-  console.log("Works");
+const onReady = () => {
+  console.log("ready");
 };
 
+// user input
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  const cityName = $("#searchInput").val();
+  console.log(cityName);
+
+  // call from API to get data
+  const currentForecast = getCurrentForecast();
+
+  console.log(currentForecast);
+};
+
+const getCurrentForecast = async () => {
+  const currentData = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=birmingham&appid=59d35777f6ec5554a91df4c08291fafc"
+  );
+
+  return currentData;
+};
+
+$(document).ready(onReady);
 searchInput.submit(handleFormSubmit);
